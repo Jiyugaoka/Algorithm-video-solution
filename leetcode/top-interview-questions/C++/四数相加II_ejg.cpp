3112a9cf53616bd35851aab935c56467f1d4dedc
ejg <jf18366102113@163.com>
2021-04-06 21:52:14 +0800

    修改四数相加II C++代码实现

--- leetcode/top-interview-questions/C++/四数相加II_ejg.cpp
+++ leetcode/top-interview-questions/C++/四数相加II_ejg.cpp
@@ -1,17 +1,69 @@
 class Solution {
 public:
+    int fourSumCount0(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        int res = 0;
+        for (int x: A) {
+            for (int y: B) {
+                for (int p: C) {
+                    for (int q: D) {
+                        if (x + y + p + q == 0) {
+                            res++;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    int fourSumCount1(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        int res = 0;
+        for (int x: A) {
+            for (int y: B) {
+                for (int p: C) {
+                    int temp = x + y + p;
+                    for (int q: D) {
+                        if (temp + q == 0) {
+                            res++;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    int fourSumCount2(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        int res = 0;
+        for (int x: A) {
+            for (int y: B) {
+                int temp1 = x + y;
+                for (int p: C) {
+                    for (int q: D) {
+                        int temp2 = p + q;
+                        if (temp1 + temp2 == 0) {
+                            res++;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
         unordered_map<int, int> countAB;
-        for (int u: A) {
-            for (int v: B) {
-                ++countAB[u + v];
+        for (int x: A) {
+            for (int y: B) {
+                countAB[x + y]++;
             }
         }
         int res = 0;
-        for (int u: C) {
-            for (int v: D) {
-                if (countAB.count(-u - v)) {
-                    res += countAB[-u - v];
+        for (int p: C) {
+            for (int q: D) {
+                int temp = p + q;
+                if (countAB.count(-temp)) {
+                    res += countAB[-temp];
                 }
             }
         }
